054796109d9365c6efae3e01296128f4309e8348
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2023-01-20 20:37:36 -0300

    chore: apply deno fmt

--- basics.ts
+++ basics.ts
@@ -1,85 +1,85 @@
 /**
  * just a simple, hard-coded message!
  */
-export const aConstValue = "hello, from deno, with love"    // the export keyword means this may be imported by other files
+export const aConstValue = "hello, from deno, with love"; // the export keyword means this may be imported by other files
 
 /**
  * a hard-coded hello!
  */
-export const hello = "hello!"
+export const hello = "hello!";
 
 /**
  * returns the "hello" const, no matter what.
  */
-export function returnsHello() {        // the export keyword may also be used for functions.
-    return hello
+export function returnsHello() { // the export keyword may also be used for functions.
+  return hello;
 }
 
 /**
  * a representation of a Living Being, which can be either alive or dead.
  */
-export class LivingBeing {      // the export keyword may also be used for classes.
-    /**
-     * flags whenever this being is alive or not. Defaults to true.
-     */
-    isAlive: boolean = true
+export class LivingBeing { // the export keyword may also be used for classes.
+  /**
+   * flags whenever this being is alive or not. Defaults to true.
+   */
+  isAlive: boolean = true;
 
-    /**
-     * mutates the living being into being dead.
-     */
-    die() {
-        this.isAlive = false
-    }
+  /**
+   * mutates the living being into being dead.
+   */
+  die() {
+    this.isAlive = false;
+  }
 }
 
 /**
  * a representation of a Person, inheriting Living Being and expanding it for human purposes.
  */
-export class Person extends LivingBeing {   // the extends keyword allows us to inherit from other classes and expand them
-    constructor(public readonly name: string) { //readonly denotes this property cannot be written to
-        super();    // ^ also note that by specifying the accessibility Typescript automatically creates this property for this class
-    }
+export class Person extends LivingBeing { // the extends keyword allows us to inherit from other classes and expand them
+  constructor(public readonly name: string) { //readonly denotes this property cannot be written to
+    super(); // ^ also note that by specifying the accessibility Typescript automatically creates this property for this class
+  }
 }
 
 /**
  * an interface describing anything that is scoreable.
  */
 export interface Scoreable {
-    /**
-     * the current score for this object.
-     */
-    get score(): number // the "get" keyword allows this to be used as a getter - no need to use () when reading from this
+  /**
+   * the current score for this object.
+   */
+  get score(): number; // the "get" keyword allows this to be used as a getter - no need to use () when reading from this
 
-    /**
-     * increases the current score.
-     */
-    increaseScore(): void
+  /**
+   * increases the current score.
+   */
+  increaseScore(): void;
 
-    /**
-     * decreases the current score.
-     */
-    decreaseScore(): void
+  /**
+   * decreases the current score.
+   */
+  decreaseScore(): void;
 }
 
 /**
  * a representation of a The Sims celebrity, extending a Person and implementing the Scoreable interface.
  */
 export class SimsCelebrity extends Person implements Scoreable {
-    get score() {
-        return this.currentScore
-    }
+  get score() {
+    return this.currentScore;
+  }
 
-    constructor(name: string, private currentScore: number) {
-        super(name);
-    }
+  constructor(name: string, private currentScore: number) {
+    super(name);
+  }
 
-    decreaseScore() {
-        let result = this.score - 1
-        this.currentScore = result < 0 ? 0 : result
-    }
+  decreaseScore() {
+    let result = this.score - 1;
+    this.currentScore = result < 0 ? 0 : result;
+  }
 
-    increaseScore() {
-        let result = this.score + 1
-        this.currentScore = result > 5 ? 5 : result
-    }
+  increaseScore() {
+    let result = this.score + 1;
+    this.currentScore = result > 5 ? 5 : result;
+  }
 }
--- basics_test.ts
+++ basics_test.ts
@@ -1,90 +1,101 @@
-import {assertEquals, assertNotEquals} from "https://deno.land/std/testing/asserts.ts"
-import {aConstValue, hello, LivingBeing, Person, returnsHello, Scoreable, SimsCelebrity} from "./basics.ts";
+import {
+  assertEquals,
+  assertNotEquals,
+} from "https://deno.land/std/testing/asserts.ts";
+import {
+  aConstValue,
+  hello,
+  LivingBeing,
+  Person,
+  returnsHello,
+  Scoreable,
+  SimsCelebrity,
+} from "./basics.ts";
 
 Deno.test("a const never changes", () => {
-    // Given
-    const expected = aConstValue    // const are immutable variables
-    let actual = aConstValue        // let or vars are mutable
+  // Given
+  const expected = aConstValue; // const are immutable variables
+  let actual = aConstValue; // let or vars are mutable
 
-    // When
-    actual = "something else"
+  // When
+  actual = "something else";
 
-    // Then
-    assertNotEquals(actual, expected)
-    assertEquals(expected, aConstValue)
-})
+  // Then
+  assertNotEquals(actual, expected);
+  assertEquals(expected, aConstValue);
+});
 
 Deno.test("functions can return something", () => {
-    // Given
-    const expected = hello
+  // Given
+  const expected = hello;
 
-    // When
-    const got = returnsHello()
+  // When
+  const got = returnsHello();
 
-    // Then
-    assertEquals(got, expected)
-})
+  // Then
+  assertEquals(got, expected);
+});
 
 Deno.test("classes can be used to organize data", () => {
-    // Given
-    const expected = false
-    const subject = new LivingBeing()
+  // Given
+  const expected = false;
+  const subject = new LivingBeing();
 
-    subject.isAlive = expected  // since "name" is a public get/set we can just write to it
+  subject.isAlive = expected; // since "name" is a public get/set we can just write to it
 
-    // When
-    const got = subject.isAlive
+  // When
+  const got = subject.isAlive;
 
-    // Then
-    assertEquals(got, expected)
-})
+  // Then
+  assertEquals(got, expected);
+});
 
 Deno.test("classes and their properties may be mutated", () => {
-    // Given
-    const expected = false
-    const subject = new LivingBeing()
+  // Given
+  const expected = false;
+  const subject = new LivingBeing();
 
-    // When
-    subject.die()   // note how this isn't reassigned anywhere
-    const got = subject.isAlive
+  // When
+  subject.die(); // note how this isn't reassigned anywhere
+  const got = subject.isAlive;
 
-    // Then
-    assertEquals(got, expected)
-})
+  // Then
+  assertEquals(got, expected);
+});
 
 Deno.test("classes may be extended (ie: inherited) by using the extends keyword", () => {
-    // Given
-    const expected = "John Doe"
-    const expectedAliveness = false
+  // Given
+  const expected = "John Doe";
+  const expectedAliveness = false;
 
-    // When
-    const person = new Person(expected)
-    person.die()
+  // When
+  const person = new Person(expected);
+  person.die();
 
-    // Then
-    assertEquals(person.name, expected)
-    assertEquals(person instanceof LivingBeing, true)
-    assertEquals(person.isAlive, expectedAliveness)
-})
+  // Then
+  assertEquals(person.name, expected);
+  assertEquals(person instanceof LivingBeing, true);
+  assertEquals(person.isAlive, expectedAliveness);
+});
 
 Deno.test("interfaces may be used to describe APIs and Data Contracts", () => {
-    // Given
-    const name = "Doe, John"
-    const startingScore = 3
-    // note how we're telling this should be treated as a Scoreable object rather than a SimsCelebrity
-    const subject: Scoreable = new SimsCelebrity(name,  startingScore)
+  // Given
+  const name = "Doe, John";
+  const startingScore = 3;
+  // note how we're telling this should be treated as a Scoreable object rather than a SimsCelebrity
+  const subject: Scoreable = new SimsCelebrity(name, startingScore);
 
-    // When
-    subject.decreaseScore()
-    const lowestScore = subject.score
-    subject.increaseScore()
-    subject.increaseScore()
-    const highestScore = subject.score
+  // When
+  subject.decreaseScore();
+  const lowestScore = subject.score;
+  subject.increaseScore();
+  subject.increaseScore();
+  const highestScore = subject.score;
 
-    // Then
-    assertEquals(lowestScore, startingScore - 1)
-    assertEquals(highestScore, startingScore + 1)
-    // assertEquals(subject.name, name)    // errors out: Scoreable doesn't have a name!
-    assertEquals(subject instanceof SimsCelebrity, true)
-    assertEquals((subject as SimsCelebrity).name, name) // works but this could error out if subject wasn't really this type
-})
+  // Then
+  assertEquals(lowestScore, startingScore - 1);
+  assertEquals(highestScore, startingScore + 1);
+  // assertEquals(subject.name, name)    // errors out: Scoreable doesn't have a name!
+  assertEquals(subject instanceof SimsCelebrity, true);
+  assertEquals((subject as SimsCelebrity).name, name); // works but this could error out if subject wasn't really this type
+});
