8776c2db2511198afa02be1eb55bf2e144171b42
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2023-01-21 16:12:58 -0300

    test: serialize and deserialize toml - with a happy linter

--- src/serialization.ts
+++ src/serialization.ts
@@ -1,15 +1,15 @@
 /**
  * representation of a game entry on a fictional website
  */
-export interface GameEntry extends Record<string, any>{
-    name: string;
-    launchedAt: Date;
+export interface GameEntry extends Record<string, unknown> {
+  name: string;
+  launchedAt: Date;
 }
 
 /**
  * a well-known game entry, useful for tests
  */
 export const defaultGameEntry: GameEntry = {
-    name: "Ring of the Elder",
-    launchedAt: new Date()
-}
+  name: "Ring of the Elder",
+  launchedAt: new Date(),
+};
--- tests/serialization_test.ts
+++ tests/serialization_test.ts
@@ -1,30 +1,36 @@
-import {assert, assertEquals} from "https://deno.land/std@0.173.0/testing/asserts.ts";
-import { parse, stringify} from "https://deno.land/std@0.173.0/encoding/toml.ts"
+import {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.173.0/testing/asserts.ts";
+import {
+  parse,
+  stringify,
+} from "https://deno.land/std@0.173.0/encoding/toml.ts";
 
-import {defaultGameEntry, GameEntry} from "../src/serialization.ts"
+import { defaultGameEntry, GameEntry } from "../src/serialization.ts";
 
 Deno.test("deno has native support to serialize objects to TOML", () => {
-    // Given
-    const subject: GameEntry = defaultGameEntry
+  // Given
+  const subject: GameEntry = defaultGameEntry;
 
-    // When
-    const got = stringify(subject);
+  // When
+  const got = stringify(subject);
 
-    // Then
-    assert(got.includes(subject.name))
-})
+  // Then
+  assert(got.includes(subject.name));
+});
 
 Deno.test("deno also has native support to parse TOML strings into objects", () => {
-    // Given
-    const subject = `
+  // Given
+  const subject = `
     name = "${defaultGameEntry.name}"
     launchedAt = ${defaultGameEntry.launchedAt.toISOString()}
-    `
+    `;
 
-    // When
-    const got = parse(subject) as GameEntry;
+  // When
+  const got = parse(subject) as GameEntry;
 
-    // Then
-    assertEquals(got.name, defaultGameEntry.name)
-    assertEquals(got.launchedAt, defaultGameEntry.launchedAt)
-})
+  // Then
+  assertEquals(got.name, defaultGameEntry.name);
+  assertEquals(got.launchedAt, defaultGameEntry.launchedAt);
+});
