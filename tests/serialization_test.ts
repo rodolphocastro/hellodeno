cab236f900c692e422cc06045342a6c0deed68ca
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2023-01-21 18:07:53 -0300

    test: serialize and deserialize base64

--- tests/serialization_test.ts
+++ tests/serialization_test.ts
@@ -1,6 +1,7 @@
 import {
   assert,
   assertEquals,
+  assertNotEquals,
 } from "https://deno.land/std@0.173.0/testing/asserts.ts";
 import {
   parse,
@@ -10,6 +11,8 @@ import {
   parse as parseYaml,
   stringify as stringYaml,
 } from "https://deno.land/std@0.173.0/encoding/yaml.ts";
+import * as base64 from "https://deno.land/std@0.173.0/encoding/base64.ts";
+
 import { defaultGameEntry, GameEntry } from "../src/serialization.ts";
 
 Deno.test("deno has native support to serialize objects to TOML", () => {
@@ -78,4 +81,20 @@ Deno.test("standard libraries also allow serializing and deserializing from JSON
   // Then
   assertEquals(gotFromJson.name, defaultGameEntry.name);
   assert(gotJson.includes(gotFromJson.name));
+});
+
+Deno.test("it's also possible to encode a string into and from base64", () => {
+  // Given
+  const aJson = JSON.stringify(defaultGameEntry);
+
+  // When
+  const gotEncoded = base64.encode(aJson);
+  const gotDecoded = base64.decode(gotEncoded);
+  const got = JSON.parse(new TextDecoder().decode(gotDecoded));
+
+  // Then
+  assertEquals(got.name, defaultGameEntry.name);
+  assertEquals(got.comments, defaultGameEntry.comments);
+  assertNotEquals(gotEncoded, null);
+  assertNotEquals(gotDecoded, null);
 });
