343d095ecc076129663660d724cc38e898a4920b
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2023-01-22 23:59:37 -0300

    test: use promise goodies

--- tests/async_test.ts
+++ tests/async_test.ts
@@ -1,4 +1,7 @@
-import { assertEquals } from "https://deno.land/std@0.173.0/testing/asserts.ts";
+import {
+  assertEquals,
+  assertNotEquals,
+} from "https://deno.land/std@0.173.0/testing/asserts.ts";
 import { doSomething, returnSomethingInUpper } from "../src/async.ts";
 
 Deno.test("deno supports async-await out of the box, simply use await on an async call to wait for it to complete", async () => {
@@ -25,4 +28,66 @@ Deno.test("deno has all the Typescript perks - thus async-await is actually a su
 
   // Then
   assertEquals(got, base.toUpperCase());
+});
+
+Deno.test("Promise.all can be used to ensure all Promises are executed", async () => {
+  // Given
+  const expected = "I did it, Adrian!";
+  let firstGot: string | undefined;
+  let secondGot: string | undefined;
+  const first = doSomething(100, () => {
+    firstGot = expected;
+  });
+  const second = doSomething(150, () => {
+    secondGot = expected;
+  });
+
+  // When
+  await Promise.all([first, second]); // note how we're actually awaiting Promise.all
+
+  // Then
+  assertEquals(firstGot, secondGot);
+  assertEquals(firstGot, expected);
+});
+
+Deno.test("Promise.any returns the first fulfilled Promise but allows others to continue executing", async () => {
+  // Given
+  const expected = "I did it again, Adrian!";
+  let firstGot: string | undefined;
+  let secondGot: string | undefined;
+  const first = doSomething(100, () => {
+    firstGot = expected;
+  });
+  const second = doSomething(250, () => {
+    secondGot = expected;
+  });
+
+  // When
+  await Promise.any([first, second]);
+
+  // Then
+  assertEquals(firstGot, expected);
+  assertNotEquals(firstGot, secondGot);
+  // workaround to give some time for the second promise to finish
+  await new Promise((r) =>
+    setTimeout(() => r(assertEquals(firstGot, secondGot)), 175)
+  );
+});
+
+Deno.test("Promise.race returns the first fulfilled Promise and rejects the others", async () => {
+  // Given
+  const expected = "I did it again, Adrian!";
+  let firstGot: string | undefined;
+  const first = doSomething(1, () => {
+    firstGot = expected;
+  });
+  const second = doSomething(2, () => {
+    throw "oh no!";
+  });
+
+  // When
+  await Promise.race([first, second]); // note how little time there is between both promises!
+
+  // Then
+  assertEquals(firstGot, expected);
 });
